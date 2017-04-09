80dfd8ab895e4919f4eedb1437e2053abe262436
weiyi.li <weiyi.just2@gmail.com>
2017-04-09 17:59:47 +0800

    [ Picasso Tutorial ] - [ Loading Image ] from a URL.
    The Picasso library is using a fluent interface, specifically implemented with the Picasso class. The Picasso class requires at least three parameters for a fully functional request:
        with(Context context);
        load(String imageUrl);
        into(ImageView targetImageView);
    
    https://futurestud.io/tutorials/picasso-getting-started-simple-loading

--- app/src/main/java/me/li2/android/tutorial/Picasso/L1ImageLoading/ImageLoading.java
+++ app/src/main/java/me/li2/android/tutorial/Picasso/L1ImageLoading/ImageLoading.java
@@ -1,8 +1,18 @@
 package me.li2.android.tutorial.Picasso.L1ImageLoading;
 
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
+
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 
@@ -51,5 +61,44 @@ public class ImageLoading extends SimpleListActivity {
     @Override
     protected void onSimpleListItemClick(int position) {
         Toast.makeText(ImageLoading.this, "Loading Image from " + mLoadingOptions.get(position), Toast.LENGTH_SHORT).show();
+        switch (position) {
+            case 0:
+                loadImageFromURL(URL);
+                break;
+        }
+    }
+
+    private static final String URL = "http://i.imgur.com/9gbQ7YR.jpg";
+
+    private void loadImageFromURL(String url) {
+        Picasso.with(this)
+                .load(url)
+                .into(showImageView());
+    }
+
+    /**
+     * Popup ImageView, give its constructor a non-dialog theme can show ImageView in full screen. and
+     * android.R.style.ThemeOverlay_Material_Dark will not change the statusBar's color.
+     * http://stackoverflow.com/a/24946375/2722270
+     * http://stackoverflow.com/a/10173576/2722270
+     */
+    private ImageView showImageView() {
+        Dialog builder = new Dialog(this, android.R.style.Theme_Material_Light_DarkActionBar);
+        builder.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        builder.getWindow().setBackgroundDrawable(
+                new ColorDrawable(Color.BLACK));
+        builder.setOnDismissListener(new DialogInterface.OnDismissListener() {
+            @Override
+            public void onDismiss(DialogInterface dialogInterface) {
+                //nothing;
+            }
+        });
+
+        ImageView imageView = new ImageView(this);
+        builder.addContentView(imageView, new RelativeLayout.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.MATCH_PARENT));
+        builder.show();
+        return imageView;
     }
 }
