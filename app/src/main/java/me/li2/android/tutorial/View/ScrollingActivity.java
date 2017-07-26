661f986e450412e37ee172e890d248be5bdc55ee
weiyi.li <weiyi.just2@gmail.com>
2017-07-26 14:11:18 +0800

    [ View ] - [ Scrolling Activity ] Fix issue that users cannot scroll the Google Map vertically with in a scrollable parent view.

--- app/src/main/java/me/li2/android/tutorial/View/ScrollingActivity.java
+++ app/src/main/java/me/li2/android/tutorial/View/ScrollingActivity.java
@@ -3,18 +3,27 @@ package me.li2.android.tutorial.View;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.widget.NestedScrollView;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnTouch;
 import me.li2.android.tutorial.R;
 
 public class ScrollingActivity extends AppCompatActivity {
+    private static final String TAG = ScrollingActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scrolling);
+        ButterKnife.bind(this);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -26,5 +35,36 @@ public class ScrollingActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+    }
+
+
+    /*
+     To fix issue that users cannot scroll the Google Map vertically with in a scrollable parent view.
+     https://stackoverflow.com/a/17317176/2722270
+     */
+
+    @BindView(R.id.rootScrollView)
+    NestedScrollView mRootScrollView;
+
+    @OnTouch(R.id.transparentInterceptView)
+    public boolean onTouch(View v, MotionEvent event) {
+        int action = event.getAction();
+        Log.d(TAG, "onTouch " + action);
+        switch (action) {
+            case MotionEvent.ACTION_DOWN:
+            case MotionEvent.ACTION_MOVE:
+                // Disallow the scrollable parent view to intercept touch events.
+                mRootScrollView.requestDisallowInterceptTouchEvent(true);
+                // Disable touch on transparent view.
+                return false;
+
+            case MotionEvent.ACTION_UP:
+                // Allow the scrollable parent view to intercept touch events.
+                mRootScrollView.requestDisallowInterceptTouchEvent(false);
+                return true;
+
+            default:
+                return true;
+        }
     }
 }
